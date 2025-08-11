02f63a89aa803d7addc6f0ff6cfc41d9e62bf5c4
Andrew Sammut <andrew.sammut@gmail.com>
2025-08-11 10:36:57 +1000

    chore: update azurerm provider version constraint

--- terraform.tf
+++ terraform.tf
@@ -9,7 +9,7 @@ terraform {
     # TODO: Ensure all required providers are listed here and the version property includes a constraint on the maximum major version.
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.71"
+      version = ">= 3.71, < 5.0"
     }
     modtm = {
       source  = "Azure/modtm"
